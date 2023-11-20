c3a4e0dbca1f0e545041bed2aff7ff2643331f55
AugustChildress <130702739+AugustChildress@users.noreply.github.com>
2023-11-20 12:35:28 -0600

    oops, forgot about deleting testing variables from bv.temp.calculation

--- BVTemps/bv.temp.calculation.py
+++ BVTemps/bv.temp.calculation.py
@@ -12,12 +12,12 @@ import matplotlib.pyplot as plt
 #10000K/T = K(B-V)+C
 
 T = [0,0,0,0,0]
-K = KC.K#1.58#slope calculated from the 
+K = 1.58#slope calculated from the 
 B = [17.03, 17.07, 16.96, 16.27, 15.86]#Blue light mag
 A_B = 0.048 #correction for the B magnitude
 V = [16.11, 16.01, 15.84, 15.22, 14.82]#Visiblle light mag
 A_V = 0.036 #correction for the V magnitude
-C = KC.C#0.48#constant based on color filters
+C = 0.48#constant based on color filters
 
 for i in range(len(T)):
     T[i] = round(10000/( K * ((B[i] - A_B) - (V[i] - A_V)) + C ),2)
