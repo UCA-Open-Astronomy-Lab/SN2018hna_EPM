d13524be901d83e64ee7c5857027ccc1b5860de3
AugustChildress <august.childress@yahoo.com>
2023-12-15 12:28:04 -0600

    Time corrected BV distance added, distances look good!

--- Main/Distance/BVDistance/BVDistanceTimeCorrection.py
+++ Main/Distance/BVDistance/BVDistanceTimeCorrection.py
@@ -1,29 +1,33 @@
-
+"""https://stackoverflow.com/questions/9990789/how-to-force-zero-interception-in-linear-regression/9994484#9994484"""
+import math
 import numpy as np
 import matplotlib.pyplot as plt
 import BVVariables as Var
 import BVDistance
 
 v = np.array(Var.v)
-print(v)
 thetaB = np.array(BVDistance.thetaB)
+thetaV = np.array(BVDistance.thetaV)
+y = np.array(Var.tdays)
 
-x = thetaB/v
-print(x)
-
-y = np.array([12.1, 15.1, 20.1, 37.2, 46.1])
-
-
-# Our model is y = a * x, so things are quite simple, in this case...
-# x needs to be a column vector instead of a 1D vector for this, however.
+#Linear regression that forces a 0 y intercept 
+x = thetaB/(v*24*60*60)
 x = x[:,np.newaxis]
 a, _, _, _ = np.linalg.lstsq(x, y,rcond=None)
 
+print()
+print("B mag time corrected BV temp distance: "+str(np.round(a/3.086E13/1000000,2))+" Mpc")
+
+#Linear regression that forces a 0 y intercept 
+x = thetaV/(v*24*60*60)
+x = x[:,np.newaxis]
+a, _, _, _ = np.linalg.lstsq(x, y,rcond=None)
+
+print()
+print("B mag time corrected BV temp distance: "+str(np.round(a/3.086E13/1000000,2))+" Mpc")
+
+'''
 plt.plot(x, y, 'bo')
-
 plt.plot(x, a*x, 'r-')
-print("Slope: ",a/3.086E13/1000000)
-
-
-"""https://stackoverflow.com/questions/9990789/how-to-force-zero-interception-in-linear-regression/9994484#9994484"""
-#plt.show()
+plt.show()
+'''
